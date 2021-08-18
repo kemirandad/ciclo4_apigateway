66f9d5fbf3ae408eab6b6fb0b5b6ad83fc5e5c90
kemirandad <kjmd2s@gmail.com>
2021-08-18 17:58:09 -0500

    Desplegando el proyecto a Heroku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-    users_api_url: process.env.USERS_API_URL || 'https://usersdbs.herokuapp.com/',
-    account_api_url: process.env.ACCOUNT_API_URL || 'http://accountsdb.herokuapp.com/',
+    users_api_url: process.env.USERS_API_URL || 'http://usersdbs.herokuapp.com',
+    account_api_url: process.env.ACCOUNT_API_URL || 'https://accountsdb.herokuapp.com',
     
 };
